2f2af2a835a767407206cefbcacbef1006dbc4e0
Daniel Di Venere <danieldivenere@gmail.com>
2020-02-27 14:45:03 -0800

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_ENDPOINT: `https://murmuring-falls-61257.herokuapp.com/api/bookmarks`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
